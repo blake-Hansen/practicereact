88207312623c29f5c22148299824d9ea87edc456
Blake Hansen <b.hansen9264@gmail.com>
2022-10-16 20:24:39 -0700

    Added MouseOver

--- app/src/index.js
+++ app/src/index.js
@@ -9,12 +9,17 @@ const books = [
 {
   img:"https://www.nbcsports.com/sites/nbcsports.com/files/styles/more_top_stories_small/public/2022/10/16/gettyimages-1433863816-e1665947641404.jpg?itok=d7AeVB5E&timestamp=1665948364",
   title:"Football",
-  author: "NBC"
+  author: "Van Dijk"
 },
 {
   img:"https://m.media-amazon.com/images/I/51XSnDfDL2L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
   title:"Wise Men Christmas",
   author: "James Wiseman"
+},
+{
+  img:"https://ichef.bbci.co.uk/live-experience/cps/480/cpsprodpb/1FF1/production/_127177180_liverpoolgoal.jpg",
+  title:"Liverpool",
+  author: "Robert Sallah"
 }
 ];
 
@@ -22,22 +27,18 @@ const books = [
 
 
 function BookList() {
-  
   return (
-    <section className="temp">
-        
+    <section className="temp">  
         {books.map( (book) => {
-          const {img, title, author} = book;
-          return <Book key={Math.random()} book={book}></Book>
+         
+          return <Book key={Math.random()} {...book}></Book>
         })}
       </section>
   )
 }
 
-const Book = (props) => {
+const Book = ({img, title, author}) => {
   const [size, setSize] = useState(30);
-  const {img, title, author} = props.book;
-  
   
   
   const bigHandler = () => {
@@ -48,11 +49,20 @@ const Book = (props) => {
     setSize(size - 5)
   }
 
+    const clickHandler = (e) => {
+      console.log(e.target.value)
+      alert("Hello")
+    }
+
+    const complexHandler = (author) => {
+        console.log(author)
+    }
   return (
-    <article className="card">
-      <h1 style={{fontSize:`${size}px`}}>{title}</h1>
+    <article className="card" onMouseOver={()=> console.log("over")}>
+      <h1 onClick={()=> console.log(title)}style={{fontSize:`${size}px`}}>{title}</h1>
       <p>{author.toUpperCase()}</p>
-     
+      <button type="button" onClick={clickHandler}>ALERT</button>
+      <button type="button" onClick={() => complexHandler(author)}>COMPLEX</button>
       <img 
       src={img}
       alt="men"
